c80c330ecc039df6846c3f2180f79ab347ee93ef
cheng.li <cheng.li@huifu.com>
2021-09-15 18:20:07 +0800

    first commit

---
+++ my-site/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e2151fa5c30fcf1660adf66caca5cce1'),
+    },
+  },
+});
---
+++ my-site/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6b5bc230-8382-4437-953e-fe5523637d79'
+};
